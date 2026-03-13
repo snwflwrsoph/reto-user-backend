c242c589746cf54910b325f89e5f20c71e71b834
snwflwrsoph <asophia.peraltar@gmail.com>
2026-03-13 12:12:29 -0700

    feat: add companyRoutes to apiRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router, Request, Response } from 'express';
 import userRoutes from './userRoutes';
+import companyRoutes from './companyRoutes';
 
 const apiRouter:Router = Router();
 
 apiRouter.use('/users', userRoutes);
+apiRouter.use('/companies', companyRoutes);
 
 apiRouter.get('/', (req:Request, res:Response) => {
     res.send('Hello World!')
