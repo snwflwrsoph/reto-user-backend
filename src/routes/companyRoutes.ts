902e38b66cd4338b181f838d969dd002c55384db
snwflwrsoph <asophia.peraltar@gmail.com>
2026-03-13 12:09:52 -0700

    feat: add company router

---
+++ src/routes/companyRoutes.ts
@@ -0,0 +1,24 @@
+import { Router, Request, Response } from 'express';
+const companyRouter:Router = Router();
+
+companyRouter.get('/', (req:Request, res:Response) => {
+    res.send('Get a list og companies')
+});
+
+companyRouter.get('/:id', (req:Request, res:Response) => {
+    res.send(`Get the product ${req.params.id}`)
+});
+
+companyRouter.post('/', (req:Request, res:Response) => {
+    res.send(`Create a new product with ID: ${req.body.id}`)
+});
+
+companyRouter.patch('/:id', (req:Request, res:Response) => {
+    res.send(`Update the product ${req.params.id} with the values of ${req.body.name}, ${req.body.tier}, ${req.body.contact} and ${req.body.address}`)
+});
+
+companyRouter.delete('/', (req:Request, res:Response) => {
+    res.send(`Deleting the product ${req.body.id}`)
+});
+
+export default companyRouter;
